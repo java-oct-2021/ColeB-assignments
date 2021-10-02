ba6edcdc148dbfc7648b5ed2546143de2d7cd8dc
Cole Beach <NFBeach50@gmail.com>
2021-10-01 19:15:26 -0700

    alfred-bot-commit

--- 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
+++ 01-java-fundamentals/02-alfred-bot/AlfredQuotes.java
@@ -1,52 +1,65 @@
-import java.util.Date;
+import java.util.Date
 
 public class AlfredQuotes {
-
-    //  Note: These greetings are not printed to the console, 
-    //        but returned as a String for use in the testing file.
-
-    // Inputs: None
-    // Return Type: String
-    // Output: Returns some basic generic greeting that Alfred might say to anyone.
+    /*
+     * Inputs: None Return Type: String Description: Returns a generic greeting that
+     * Alfred will say to anyone.
+     */
     public String basicGreeting() {
-        return "Hello, lovely to see you. Isn't it nice out today?";
+        // You do not need to code here, this is an example method
+        return "Hello Master Bruce, happy to see you lived another night. Are needing any tea?";
     }
 
-    // Inputs: String name, String - assume "morning", "afternoon" or "evening".
-    // Return Type: String
-    // Output: Returns a greeting alfred might say, 
-    //         that includes the person's name in the greeting.
+    /*
+     * Inputs: String name, String dayPeriod Assume "morning", "afternoon" or
+     * "evening". Return Type: String Description: Returns a greeting that includes
+     * the person's name as well as the day period. For example:
+     * "Good evening, Beth Kane. Lovely to see you." Tip: Try using the
+     * String.format method for string interpolation in Java.
+     */
     public String guestGreeting(String name, String dayPeriod) {
-        return String.format("Good %s, %s. Lovely to see you.", dayPeriod, name);
-       //format is the same an f string in python only you're spelling out format here 
+
+        return String.format("Good %s, %s. I take it you would like me to cancel all of your meetings.", dayPeriod,
+                name);
     }
 
-    // Inputs: None
-    // Return Type: String
-    // Output: Returns an announcement of the current date, 
-    //         in a polite manner.
+    /*
+     * Inputs: None Return Type: String Description: Returns a polite announcement
+     * of the current date.
+     */
     public String dateAnnouncement() {
-        return String.format("It is currently %s", new Date());
-    }
-    
-    // Inputs: String (Any phrase)
-    // Return Type: String
-    // Output: A repsonse (String)
-    public String answeringBeforeAlexis(String phrase) {
-
-        if(phrase.indexOf("Alexis") > -1) {
-            return "She's really of no help. What can I get for you?";
-        }
-
-        if (phrase.indexOf("Alfred") > -1) {
-            return "At your service, naturally. How may I be of assistance?";
-        }
-
-        return "Right. And with that I shall retire.";
+        // YOUR CODE HERE
+        return String.format("If you must know, today is %s. The anniversary of your parents death...", new Date());
     }
 
+    /*********************************************
+     * Final Challenge! Alfred is listening. Write a method that accepts any string
+     * of conversation. If "Alexis" appears in the conversation return a snarky
+     * response, such as: "Right away, sir. She certainly isn't sophisticated enough
+     * for that."
+     * 
+     * If "Alfred" is in the conversation return an obliging response, for example:
+     * "At your service. As you wish, naturally."
+     * 
+     * If neither name is found, return an appropriate response, for instance:
+     * "Right. And with that I shall retire."
+     **********************************************/
+    /*
+     * Inputs: String (A conversation) Return Type: String (Something Alfred would
+     * say in response)
+     * 
+     * Tip: Use the indexOf String method
+     */
+    public String respondBeforeAlexis(String summoned) {
+        if (summoned.indexOf("Alexis") > -1) {
+            return "Do not trust Robots like her, I can be of better assistance!";
+        }
+        if (summoned.indexOf("Alfred") > -1) {
+            return "Ah, Master Bruce, how can I be of assistance?";
+        }
+        return "I'd be happy to put on the Batsuit and do it myself.";
+    }
     // NINJA BONUS
-    // Write your own AlfredQuote method using any of the String methods you have learned.
-
-
+    // Write your own AlfredQuote method using any of the String methods you have
+    // learned!
 }
--- 01-java-fundamentals/02-alfred-bot/AlfredTest.java
+++ 01-java-fundamentals/02-alfred-bot/AlfredTest.java
@@ -1,30 +1,25 @@
 public class AlfredTest {
     /*
-    * This main method will always be the launch point for a Java application
-    * For now, we are using the main to test all our 
-    * AlfredQuotes methods.
-    */
+     * This main method will always be the launch point for a Java application For
+     * now, we are using the main to test all our AlfredQuotes methods.
+     */
     public static void main(String[] args) {
         // Make an instance of AlfredQuotes to access all it's methods.
         AlfredQuotes alfredBot = new AlfredQuotes();
-        
+
         // Make some test greetings, providing any necessary data
         String testGreeting = alfredBot.basicGreeting();
-        String testGuestGreeting = alfredBot.guestGreeting("Beth Kane", "evening");
+        String testGuestGreeting = alfredBot.guestGreeting("Bruce Wayne", "morning");
         String testDateAnnouncement = alfredBot.dateAnnouncement();
-        
-        String alexisTest = alfredBot.respondBeforeAlexis(
-                            "Alexis! Play some low-fi beats."
-                            );
-        String alfredTest = alfredBot.respondBeforeAlexis(
-            "I can't find my yo-yo. Maybe Alfred will know where it is.");
-        String notRelevantTest = alfredBot.respondBeforeAlexis(
-            "Maybe that's what Batman is about. Not winning. But failing.."
-        );
-        
+
+        String alexisTest = alfredBot.respondBeforeAlexis("Alexis! Play some Grime.");
+        String alfredTest = alfredBot.respondBeforeAlexis("Alfred! Put on Grime and fight my crimes");
+        String notRelevantTest = alfredBot
+                .respondBeforeAlexis("I'm not the hero they wanted but the one they are getting!");
+
         // Print the greetings to test.
         System.out.println(testGreeting);
-        
+
         // Uncomment these one at a time as you implement each method.
         // System.out.println(testGuestGreeting);
         // System.out.println(testDateAnnouncement);
